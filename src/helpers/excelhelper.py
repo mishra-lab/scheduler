2f918abf7aec200aa5ade8c8bb450d81da50a1ea
David Landsman <david.landsman@mail.utoronto.ca>
2018-12-04 14:00:59 -0500

    Add support for weekends in monthly export
    * Modify `expand_dates` helper to enumerate weekends as well as weekdays
    * Clean up some of the code in `saveMonthlySchedule` by introducing a DayFields namedtuple
    * Add helpers for retrieving table header fields and multiple columns (as tuples)
    * Add some additional comments and documentation to `saveMonthlySchedule`

--- src/helpers/excelhelper.py
+++ src/helpers/excelhelper.py
@@ -2,10 +2,11 @@ from datetime import datetime, timedelta
 import itertools
 import operator
 import calendar
+from collections import namedtuple
 
 from openpyxl import Workbook
 
-from constants import WEEK_HOURS
+from constants import WEEK_HOURS, WEEKEND_HOURS
 
 
 class ExcelHelper:
@@ -36,30 +37,67 @@ class ExcelHelper:
     def saveMonthlySchedule(filename, table, year):
         # for each weeknum, loop over days in that week, assuming we start
         # at monday and end on friday
+        
+        # TODO: add time of day field (to distinguish 8am-5pm from 5pm-8am)
+        DayFields = namedtuple('DayFields', ['fulldate', 'date', 'day', 'clinicians'])
 
-        def expand_dates(weekText, clinician):
+        cal = calendar.Calendar()
+        numCols = table.columnCount()
+        divisions = ExcelHelper.getHorizontalHeaders(table)[1:-1]
+        weekNums = ExcelHelper.getColumn(table, 0)
+        clinicianTuples = ExcelHelper.getColumns(table, [i for i in range(1, numCols)])
+
+        def expand_dates(weekText, clinTuple):
+            '''
+            Given a week number and a set of clinicians that cover the corresponding \
+            week + weekend, return a mapping between weekday and clinician(s) covering that day
+            '''
+
             result = []
             weekNum = int(
                 weekText[:-1]) if weekText[-1] == '*' else int(weekText)
 
-            d = weekStart = datetime.strptime(
+            # take the last clinician to be the one on-call for the weekend
+            weekClinicians = clinTuple[:-1]
+            weekendClinician = tuple([clinTuple[-1]] * len(divisions))
+
+            # TODO: handle holiday weekends
+            day = weekStart = datetime.strptime(
                 # year / weekNum / Monday / 8AM
                 '{0}/{1:02d}/1/08:00'.format(year, weekNum),
                 '%G/%V/%u/%H:%M'
             )
             weekEnd = weekStart + timedelta(hours=WEEK_HOURS)
 
-            while d <= weekEnd:
-                # TODO: enumerate all divisions
-                result.append(('{:%Y-%m-%d}'.format(d), '{:%b-%d}'.format(d), '{:%a}'.format(d), clinician))
-                d += timedelta(days=1)
+            # enumerate weekdays
+            while day <= weekEnd:
+                result.append(DayFields(
+                    '{:%Y-%m-%d}'.format(day), 
+                    '{:%b-%d}'.format(day), 
+                    '{:%a}'.format(day), weekClinicians))
+                day += timedelta(days=1)
+
+            # enumerate weekend
+            nextStart = weekEnd + timedelta(hours=WEEKEND_HOURS)
+            while day < nextStart:
+                result.append(DayFields(
+                    '{:%Y-%m-%d}'.format(day), 
+                    '{:%b-%d}'.format(day), 
+                    '{:%a}'.format(day), 
+                    weekendClinician))
+                day += timedelta(days=1)
 
             return result
 
-        cal = calendar.Calendar()
-
         def group_months(tup):
-            date = tup[0]
+            '''
+            Given a DayFields tuple, figure out which month it belongs in.
+
+            Note: months will always have complete weeks, so this will group together dates
+            that may be outside of a certain month (e.g.: if Dec 31 is a Monday, it will be grouped together
+            with January)
+            '''
+            date = tup.fulldate
             
             for i in range(1, 13):
                 # search for date in month iterator
@@ -69,14 +107,13 @@ class ExcelHelper:
                 if date in month_dates:
                     return (year, i)
 
-        weekNums = ExcelHelper.getColumn(table, 0)
-        clinicians = ExcelHelper.getColumn(table, 1)
+        # create clinician mapping for each day of the year based on week numbers
+        date_maps = list(map(expand_dates, weekNums, clinicianTuples))
 
-        date_maps = list(map(expand_dates, weekNums, clinicians))
+        # group day mappings according to month
         all_dates = list(itertools.chain.from_iterable(date_maps))
-
-        # group according to month
         groupby_months = itertools.groupby(all_dates, group_months)
+
         month_dict = {k : list(v) for k, v in groupby_months}
         keys = sorted(month_dict.keys(), key=operator.itemgetter(0, 1))
 
@@ -85,17 +122,27 @@ class ExcelHelper:
         for key in keys:
             title = calendar.month_name[key[1]]
             ws = wb.create_sheet(title)
+
             # write header
             ws.cell(row=1, column=1, value='Date')
             ws.cell(row=1, column=2, value='Day')
-            ws.cell(row=1, column=3, value='%%DIV 1%%') #TODO: enumerate all divisions
 
+            col_idx = 3
+            for div in divisions:
+                ws.cell(row=1, column=col_idx, value='{} Service'.format(div))
+                col_idx += 1
+
+            # write month sheet
             month = month_dict[key]
             for i in range(len(month)):
-                day = month[i][1:]
-                print(day)
-                for j in range(len(day)):
-                    ws.cell(row=(i + 2), column=j + 1, value=str(day[j]))
+                daytuple = month[i]
+                ws.cell(row=(i+2), column=1, value=daytuple.date)
+                ws.cell(row=(i+2), column=2, value=daytuple.day)
+
+                col_idx = 3
+                for div in daytuple.clinicians:
+                    ws.cell(row=(i+2), column=col_idx, value=div)
+                    col_idx += 1
 
         wb.save(filename)
 
@@ -109,10 +156,32 @@ class ExcelHelper:
         return column
 
     @staticmethod
+    def getColumns(table, col_idxs):
+        rows = table.rowCount()
+        result = []
+        for i in range(rows):
+            row = []
+            for j in col_idxs:
+                row.append(table.item(i, j).text())
+
+            result.append(tuple(row))
+
+        return result
+
+    @staticmethod
     def getRow(table, row_idx):
-        cols = table.rowCount()
+        cols = table.columnCount()
         row = []
         for i in range(cols):
             row.append(table.item(row_idx, i).text())
 
         return row
+
+    @staticmethod
+    def getHorizontalHeaders(table):
+        cols = table.columnCount()
+        headers = []
+        for i in range(cols):
+            headers.append(table.horizontalHeaderItem(i).text())
+
+        return headers
