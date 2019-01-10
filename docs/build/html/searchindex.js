f32311ee6d116e0833d711f847c37987bff52b26
David Landsman <david.landsman@mail.utoronto.ca>
2019-01-10 13:46:31 -0500

    Add docs/build/html folder

---
+++ docs/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to Clinician Scheduler\u2019s documentation!"],titleterms:{clinician:0,document:0,indic:0,schedul:0,tabl:0,welcom:0}})
